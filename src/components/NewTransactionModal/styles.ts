a1d5e3a2a563119b883e27b70d6d1225227b0ef5
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-20 15:22:19 -0300

    adiciona cor suave ao botao selecionada no input

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
+
 
 export const Container = styled.form`
     h2{
@@ -55,13 +56,29 @@ export const TransactionTypeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+`;
 
-    button{
+
+interface RadioBoxProps{
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E40'
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
         height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: transparent;
+        background: ${(props)=> props.isActive 
+        ? transparentize(0.9, colors[props.activeColor]) // 90% transparente
+        : 
+        'transparent'
+        };
         display: flex;
         justify-content: center;
         align-items: center;
@@ -83,5 +100,4 @@ export const TransactionTypeContainer = styled.div`
             font-size: 1rem;
             color: var(--text-title);
         }
-    }
 `;
