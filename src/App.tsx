47cd258e29125a707376252ffc1368e848de9906
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-20 15:00:51 -0300

    cria estilizacao e modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './global';
 
+Modal.setAppElement("#root"); //Esse uso é único para acessibilidade.
+
 export function App() {
+
+  const[ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true); 
+  }
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false); 
+  }
+
   return (
     <>
-     <Header />
+     <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
      <Dashboard />
+
+     <NewTransactionModal 
+     isOpen={isNewTransactionModalOpen} 
+     onRequestClose={handleCloseNewTransactionModal} />
+
      <GlobalStyle />
     </>
   );
